031d28e2b2b767bcf0bf6bb00576fa65f96c3b67
DAASDI <138684304+DAASDI@users.noreply.github.com>
2024-12-17 10:15:32 +0100

    leçon 10

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,9 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_or
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
-    session.sql(my_insert_stmt).collect()
+    #session.sql(my_insert_stmt).collect()
+    import requests
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
+
     st.success('Your Smoothie is ordered!',  icon="✅")
